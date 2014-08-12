4bf03724715fd17cd1a1bd4f73304b0f502e3236
Jeff Burgess <jeffreydavidburgess@gmail.com>
2014-08-12 10:55:36 -0700

    Update content-include.php

--- content-include.php
+++ content-include.php
@@ -1,6 +1,17 @@
 
 // used to insert and swap content includes using query string variables in hyperlinks
 
+
+<ul class="php-menu">
+<li><a href="/snippets/php-includes.php">Home</a></li>
+<li><a href="/snippets/php-includes.php?pg=one">Include One</a></li>
+<li><a href="/snippets/php-includes.php?pg=two">Include Two</a></li>
+<li><a href="/snippets/php-includes.php?pg=three" class="last">Include Three</a></li>
+</ul>
+
+
+
+
 <?php
 
 if (isset($_GET['pg']) && $_GET['pg'] != "") {
